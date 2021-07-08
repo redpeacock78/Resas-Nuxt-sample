128d7d65bae9c375114bdfa6eff38336a078a74a
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-08 12:50:18 +0900

    feat: Add components/Header.vue

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header id="header">RESAS-API-Nuxt.js</header>
+</template>
+
+<style>
+header {
+  width: 100%;
+  height: 55px;
+  color: #ffffff;
+  background: #007bbb;
+  font-weight: normal;
+  font-size: 25px;
+  padding-left: 20px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  top: 0;
+  box-shadow: 1px 1px 2px #ccc;
+}
+</style>
