967cfe1e18d1c5920fd3beb06ffef9e483e0129f
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-08 12:17:16 +0900

    chore: Fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,21 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    node: true,
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
   ],
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+  },
 }
