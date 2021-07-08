692893a6ad1c8939d9f05bc257293eee923d794c
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-08 13:57:32 +0900

    fix: plugins/axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,7 @@
 import { cacheAdapterEnhancer } from 'axios-extensions'
+export let axios
 
 export default function ({ $axios }) {
   $axios.defaults.adapter = cacheAdapterEnhancer($axios.defaults.adapter)
+  axios = $axios
 }
