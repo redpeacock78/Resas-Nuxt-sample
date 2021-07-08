c8a12bbc7298283414589862fffd086cdd26adad
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-08 18:21:28 +0900

    feat: Add getPrefPopulation() to plugins/modules/resas_api.js

--- plugins/modules/resas_api.js
+++ plugins/modules/resas_api.js
@@ -11,4 +11,21 @@ export default {
       }
     )
   },
+  getPrefPopulation(prefData) {
+    const URL = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefData.prefCode}`
+    const name = prefData.prefName
+    return axios
+      .$get(URL, {
+        headers: {
+          'X-API-KEY': process.env.API_KEY,
+        },
+      })
+      .then((resp) => {
+        return {
+          label: name,
+          data: resp.result.data[0].data.map((data) => data.value),
+          fill: false,
+        }
+      })
+  },
 }
