d5f62110d11f2626eb752e9b11293cb4fc5dca40
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-09 21:41:54 +0900

    fix: fix components/CheckPrefList.vue

--- components/CheckPrefList.vue
+++ components/CheckPrefList.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="checkbox-parent">
-    <v-card-title id="checkbox-title">都道府県</v-card-title>
-    <v-card-text id="checkboxes">
-      <v-checkbox
+    <div id="checkbox-title">都道府県</div>
+    <div id="checkboxes">
+      <div
+        class="checkbox"
         v-for="(item, index) in prefData.result"
         :key="index"
-        class="checkbox-items"
-        :label="item.prefName"
-        v-bind:value="item"
-        v-model="checkedPrefData"
-        @change="check"
-      />
-    </v-card-text>
+      >
+        <input
+          type="checkbox"
+          class="checkbox-items"
+          :id="'pref' + index"
+          :value="item"
+          v-model="checkedPrefData"
+          @change="check"
+        />
+        <label :for="'pref' + index">
+          {{ item.prefName }}
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,32 +43,87 @@ export default {
 #checkbox-parent {
   margin: 2em auto;
   padding: 1em;
-  width: 90%;
+  width: 87.5%;
   border: 1px solid #ccc;
   background-color: #fff;
   box-shadow: 1px 1px 2px #ccc;
   text-align: center;
 }
+#checkbox-title {
+  display: flex;
+  padding: 16px;
+  color: #828c9a;
+  font-size: 1.25rem;
+  font-weight: normal;
+  letter-spacing: 0.0125em;
+  line-height: 2rem;
+}
 #checkboxes {
-  display: -webkit-flex;
-  display: -moz-flex;
-  display: -ms-flex;
-  display: -o-flex;
   display: flex;
   flex-flow: row wrap;
+  padding: 16px;
+  padding-left: 15px;
+  padding-top: 0;
 }
-#checkbox-title {
+.checkbox {
+  text-align: left;
+  margin-top: 19.5px;
+}
+.checkbox label {
+  display: block;
+  float: left;
+  width: 75px;
+  margin-bottom: 2.5px;
   color: #828c9a;
+  font-size: 16px;
   font-weight: normal;
 }
-.checkbox-items {
-  font-size: 18px;
-  font-weight: normal;
-  color: #828c9a;
-  width: 100px;
-  height: 30px;
-  justify-content: flex-start;
+.checkbox input[type='checkbox'] {
+  display: none;
+}
+.checkbox input[type='checkbox'] + label {
+  display: none;
   cursor: pointer;
+  display: inline-block;
   position: relative;
+  padding-left: 26px;
+}
+.checkbox input[type='checkbox'] + label::before {
+  content: '';
+  position: absolute;
+  display: block;
+  box-sizing: border-box;
+  width: 18px;
+  height: 18px;
+  margin-top: -9px;
+  margin-left: 5px;
+  left: 0;
+  top: 50%;
+  border: 2px solid;
+  border-radius: 20%;
+  border-color: #808080;
+  background-color: #fff;
+}
+.checkbox input[type='checkbox']:checked + label::before {
+  background-color: #808080;
+}
+.checkbox input[type='checkbox'] + label::after {
+  content: '';
+  position: absolute;
+  display: block;
+  box-sizing: border-box;
+  width: 12px;
+  height: 6.5px;
+  margin-top: -5px;
+  top: 50%;
+  left: 8px;
+  transform: rotate(-47deg);
+  border-bottom: 2.5px solid;
+  border-left: 2.5px solid;
+  border-color: #fff;
+  opacity: 0;
+}
+.checkbox input[type='checkbox']:checked + label::after {
+  opacity: 1;
 }
 </style>
